219fe86e87c98c31ff44c86f4965d11046271d59
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-08-21 20:34:42 +0900

    add ecs関連のロールの付与

--- iam/aws_iam_role.tf
+++ iam/aws_iam_role.tf
@@ -1,6 +1,13 @@
-# IAMユーザにアクセス権限を委任するロールの作成
+# インスタンス付与するロールの作成
 resource "aws_iam_role" "sample_ecs_instance_role" {
   name = "sample_ecs_instance_role"
   path = "/"
   assume_role_policy = file("aws_iam_role_policies/ec2_assume_role_policy.json")
+}
+
+# ecs taskに付与するロールの作成
+resource "aws_iam_role" "sample_ecs_task_role" {
+  name = "sample-ecs-task-role"
+  path = "/"
+  assume_role_policy = file("aws_iam_role_policies/ecs_task_assume_role_policy.json")
 }
